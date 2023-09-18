2dad0a28682896f1b15aaa14ae8c3f1d6b74ff49
Billy Euceda <billymm314@gmail.com>
2023-09-18 16:48:41 -0700

    newwwwww

--- infra/bucket.tf
+++ infra/bucket.tf
@@ -1,20 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-state-resume"
-    key    = "terraform/state/terraform.tfstate"
-    region = "us-west-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt = true
-  }
-}
-
 provider "aws" {
   region = "us-west-1"
 }
 
 #* Domain Bucket
 resource "aws_s3_bucket" "this" {
-  bucket      = "github-actions-practice-314"
+  bucket = "github-actions-practice-314"
 }
 
 #* Domain Bucket Index File
